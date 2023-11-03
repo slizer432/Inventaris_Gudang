d5a9c3a6771f92f83465c82690bd1477d3f3c528
Rayhan Heru Arifiansyah <rayhanheruk@gmail.com>
2023-11-03 10:03:06 +0700

    update variable gudang mjd array dan case 1 & 2

--- InventarisTest.java
+++ InventarisTest.java
@@ -7,25 +7,29 @@ public class InventarisTest {
     Scanner sc = new Scanner(System.in);
     boolean logIn = false;
     int pilihan;
-    String gudang1 = "Kosong"; 
-    String gudang2 = "Kosong";
-    String gudang3 = "Kosong";
+    String gudang1[][] = new String[15][5];
+    String gudang2[][] = new String[15][5];
+    String gudang3[][] = new String[15][5];
     String statusBarang1 = "Kosong";
     String statusBarang2 = "Kosong";
     String statusBarang3 = "Kosong";
     String barang;
-    while (logIn == false) {
-        System.out.print("Masukkan username: ");
-        String username = sc.nextLine();
-        System.out.print("Masukkan password: ");
-        String password = sc.nextLine(); 
-        if (username.equalsIgnoreCase("Admin") && password.equals("12345")) {
-            System.out.println("Log in berhasil");
-            logIn = true;
-        }else {
-            System.out.println("Log in gagal");        
+
+    while (true) {
+        
+        // sistem log in
+        while (logIn == false) {
+            System.out.print("Masukkan username: ");
+            String username = sc.nextLine();
+            System.out.print("Masukkan password: ");
+            String password = sc.nextLine(); 
+            if ((username.equals("Admin1") || username.equals("Admin2") || username.equals("Admin3")) && password.equals("12345")) {
+                System.out.println("Log in berhasil");
+                logIn = true;
+            }else {
+                System.out.println("Log in gagal");        
+            }
         }
-    }
     // menu utama
     while (true) {
         System.out.println("-------------------------");
@@ -51,25 +55,52 @@ public class InventarisTest {
             int pilihanGudang = sc.nextInt();
             switch (pilihanGudang) {
                 case 1:
-                    gudang1 = barang;
+                    for (int i = 0; i < gudang1.length; i++) {
+                        if (gudang1[i][0] != null) {
+                            continue;
+                        }
+                        gudang1[i][0] = barang;
+                        break;
+                    }
                     break;
-                case 2:
-                    gudang2 = barang;
+                    case 2:
+                    for (int i = 0; i < gudang2.length; i++) {
+                        if (gudang1[i][0] != null) {
+                            continue;
+                        }
+                        gudang2[i][0] = barang;
+                        break;
+                    }
                     break;
-                case 3:
-                    gudang3 = barang;
-                default:
+                    case 3:
+                    for (int i = 0; i < gudang3.length; i++) {
+                        if (gudang3[i][0] != null) {
+                            continue;
+                        }
+                        gudang3[i][0] = barang;
+                        break;
+                    }
+                    default:
                     System.out.println("Masukan tidak valid");
-                break;
+                    break;
             }break;
             // sistem lihat gudang
             case 2:
             System.out.println("Inventaris");
-            System.out.println("Gudang 1 : " + gudang1);
-            System.out.println("Gudang 2 : " + gudang2);
-            System.out.println("Gudang 3 : " + gudang3);
+            System.out.print("Gudang 1: ");
+            for (int i = 0; i < gudang1.length; i++) {
+                System.out.print(gudang1[i][0] + ", ");
+            }
+            System.out.print("\nGudang 2: ");
+            for (int i = 0; i < gudang2.length; i++) {
+                System.out.print(gudang2[i][0] + ", ");
+            }
+            System.out.print("\nGudang 3: ");
+            for (int i = 0; i < gudang3.length; i++) {
+                System.out.print(gudang3[i][0] + ", ");
+            }System.out.print("\n");
             break;
-                
+            
             default:
             System.out.println("Pilihan tidak valid");
             break;
@@ -78,18 +109,18 @@ public class InventarisTest {
             System.out.print("Pilih nomer gudang yang ingin dikosongkan (1,2,3): ");
             int pilihanG = sc.nextInt();
             switch (pilihanG) {
-                case 1:
-                    gudang1 = "Kosong";
-                    break;
-                case 2:
-                    gudang2 = "Kosong";
-                    break;
-                case 3:
-                    gudang3 = "Kosong";
-                    break;
-                default:
-                    System.out.println("Masukan tidak valid");
-                    break;
+                // case 1:
+                // gudang1 = "Kosong";
+                // break;
+                // case 2:
+                // gudang2 = "Kosong";
+                // break;
+                // case 3:
+                // gudang3 = "Kosong";
+                // break;
+                // default:
+                // System.out.println("Masukan tidak valid");
+                // break;
             }break;
             // sistem manajemen barang
             case 4:
@@ -100,34 +131,35 @@ public class InventarisTest {
                 String update = sc.nextLine();
                 switch (pilihanBarang) {
                     case 1:
-                        statusBarang1 = update;
-                        break;
+                    statusBarang1 = update;
+                    break;
                     case 2:
-                        statusBarang2 = update;
-                        break;
+                    statusBarang2 = update;
+                    break;
                     case 3:
                         statusBarang3 = update;
                         break;
-                    default:
+                        default:
                         System.out.println("masukan tidak valid");
                         break;
-            // sistem lihat status barang
+                        // sistem lihat status barang
                     }
-            case 5:
-            System.out.println("Kondisi barang");
-            System.out.println("Gudang 1: " + statusBarang1);
-            System.out.println("Gudang 2: " + statusBarang2);
-            System.out.println("Gudang 3: " + statusBarang3);
-            break;
-
-            case 6:
-
-
-            // sisten untuk keluar
-            case 7:
-            System.exit(0);
-        }
-        
-      }
+                    case 5:
+                    System.out.println("Kondisi barang");
+                    System.out.println("Gudang 1: " + statusBarang1);
+                    System.out.println("Gudang 2: " + statusBarang2);
+                    System.out.println("Gudang 3: " + statusBarang3);
+                    break;
+                    
+                    case 6:
+                    
+                    
+                    // sisten untuk keluar
+                    case 7:
+                    System.exit(0);
+                }
+            }
+            
     }
-  }
+}
+}
